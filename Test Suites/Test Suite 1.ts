008a4364be76392ee5f2b81068e25873fe5f7c16
spanda04 <spanda04@sacvd04283.bsc.bscal.com>
2020-04-06 17:50:25 +0530

    First Commit

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>03f7f231-2510-4388-8692-5e383ef3096d</testSuiteGuid>
+   <testCaseLink>
+      <guid>65f14d5c-9f33-4757-aafb-8978c773065c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>98d47b66-830e-4088-a079-35c1d32dbb86</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHttpMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
